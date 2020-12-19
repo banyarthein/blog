6cd293045bed5bbd614824eee9bb37a8c7dbcefc
banyarthein <banyar.lanwork@gmail.com>
2020-12-19 13:34:21 +0800

    test commit

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,24 +1,10 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import CommentCreate from './CommentCreate';
+import React from 'react';
 
-export default (props) => {
-
-    const { postId } = props;
-    const [comments, setComments] = useState({});
-
-    const fetchData = async () => {
-        const response = await axios.get(`http://localhost:4001/posts/${postId}/comments`)
-        setComments(response.data);
-    }
-
-    useEffect(() => {
-        fetchData();
-    }, []);
+export default ({ comments }) => {
 
     console.log(comments);
 
-    const renderedComments = Object.values(comments).map(comment => {
+    const renderedComments = comments.map(comment => {
         return (
             <li key={comment.id}> {comment.content} </li>
         );
